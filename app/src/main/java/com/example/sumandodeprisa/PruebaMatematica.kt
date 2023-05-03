ddcecad561853fb2c35d81896858cc36c238493b
Pablo Marian <pablomarianv@gmail.com>
2023-05-04 00:15:24 +0200

    Prueba matematica Layout F

--- app/src/main/java/com/example/sumandodeprisa/PruebaMatematica.kt
+++ app/src/main/java/com/example/sumandodeprisa/PruebaMatematica.kt
@@ -2,10 +2,15 @@ package com.example.sumandodeprisa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 
 class PruebaMatematica : AppCompatActivity() {
+    lateinit var pB: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prueba_matematica)
+
+        pB = findViewById(R.id.pB)
+
     }
 }
